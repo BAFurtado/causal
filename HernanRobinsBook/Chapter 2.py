eeef960e7327f050deeb93806078f16120b7c580
bafurtado <furtadobb@gmail.com>
2020-08-07 15:18:32 -0300

    basics ch.2

--- HernanRobinsBook/Chapter 2.py
+++ HernanRobinsBook/Chapter 2.py
@@ -1,12 +1,20 @@
 import pandas as pd
 
 
-def prob_given_treatment(data, group):
-    temp = data[data.A == group]
-    print(temp.Y.sum()/len(temp))
+def prob_given_treatment(data, treatment, outcome, group):
+    temp = data[data[treatment] == group]
+    prob = temp[outcome].sum() / len(temp)
+    print(f'{prob:.4f}')
+    return temp
 
 
 if __name__ == '__main__':
+    # 'A': received heart transplant -- treatment or not
+    # 'Y': ouctome -- died or not
+    # 'L': prognostic -- critical condition or not
     df = pd.read_csv('table2.1.csv', sep=';')
-    prob_given_treatment(df, 1)
-    prob_given_treatment(df, 0)
+    prob_given_treatment(df, 'A', 'Y', 1)
+    prob_given_treatment(df, 'A', 'Y', 0)
+
+    df2 = pd.read_csv('table2.2.csv', sep=';')
+    # Causal risk ration Pr[Ya=1 = 1]/Pr[Ya=0 = 1]
